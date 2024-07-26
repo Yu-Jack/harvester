a9cbf1bca8913427a9b64f5be9c4954315314c8a
Jack Yu <jack.yu@suse.com>
2024-07-26 22:47:44 +0800

    comment: add explaination
    Signed-off-by: Jack Yu <jack.yu@suse.com>

--- tests/integration/api/suite_test.go
+++ tests/integration/api/suite_test.go
@@ -110,6 +110,7 @@ func TestAPI(t *testing.T) {
 
 var _ = SynchronizedBeforeSuite(
 	func() []byte {
+		// Only first processor run this function
 		testCtx, testCtxCancel = context.WithCancel(context.Background())
 		var err error
 
@@ -164,6 +165,9 @@ var _ = SynchronizedBeforeSuite(
 		MustNotError(err)
 		return b
 	}, func(combinedConf []byte) {
+		// All processors run this function
+
+		// combinedConf is the return value from previous function
 		err := json.Unmarshal(combinedConf, &combinedConfig)
 		MustNotError(err)
 
