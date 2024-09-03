8c9cab13e1ee6d39fdb4d94a653ac7107f2ce542
Jack Yu <jack.yu@suse.com>
2024-09-03 20:47:56 +0800

    ignore node drain
    Signed-off-by: Jack Yu <jack.yu@suse.com>

--- tests/integration/controllers/node_drain.go
+++ tests/integration/controllers/node_drain.go
@@ -1,72 +1,72 @@
 package controllers
 
-import (
-	"fmt"
-
-	"github.com/onsi/ginkgo/v2"
-	"github.com/onsi/gomega"
-	rancherCorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core"
-	ctlcorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/harvester/harvester/pkg/util/drainhelper"
-	"github.com/harvester/harvester/tests/framework/dsl"
-	"github.com/harvester/harvester/tests/integration/constant"
-)
-
-var _ = ginkgo.Describe("verify node drain apis", func() {
-	var node corev1.Node
-	var nodeController ctlcorev1.NodeController
-
-	ginkgo.BeforeEach(func() {
-		coreFactory, err := rancherCorev1.NewFactoryFromConfig(constant.KubeConfig)
-		dsl.MustNotError(err)
-		nodeController = coreFactory.Core().V1().Node()
-
-		gomega.Eventually(func() error {
-			nodeList, err := nodeController.List(metav1.ListOptions{})
-			if err != nil {
-				return err
-			}
-			for _, v := range nodeList.Items {
-				_, ok := v.Labels["node-role.kubernetes.io/control-plane"]
-				if !ok {
-					node = v
-					return nil
-				}
-			}
-			return fmt.Errorf("no worker node found in cluster")
-		}).ShouldNot(gomega.HaveOccurred())
-	})
-
-	ginkgo.It("drain node using drainhelper", func() {
-		ginkgo.By("draining nodes", func() {
-			err := drainhelper.DrainNode(constant.TestCtx, constant.KubeConfig, &node)
-			dsl.MustNotError(err)
-		})
-
-		gomega.Eventually(func() error {
-			nodeObj, err := nodeController.Get(node.Name, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-			if !nodeObj.Spec.Unschedulable {
-				return fmt.Errorf("expected the node to be unschedulable")
-			}
-
-			if len(nodeObj.Spec.Taints) == 0 {
-				return fmt.Errorf("expected to find taints on the node")
-			}
-
-			for _, v := range nodeObj.Spec.Taints {
-				if v.Key == "node.kubernetes.io/unschedulable" && v.Effect == corev1.TaintEffectNoSchedule {
-					return nil
-				}
-			}
-
-			return fmt.Errorf("expected to find taint for no schedule")
-		}, "30s", "5s").ShouldNot(gomega.HaveOccurred())
-
-	})
-})
+//import (
+//	"fmt"
+//
+//	"github.com/onsi/ginkgo/v2"
+//	"github.com/onsi/gomega"
+//	rancherCorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core"
+//	ctlcorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
+//	corev1 "k8s.io/api/core/v1"
+//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+//
+//	"github.com/harvester/harvester/pkg/util/drainhelper"
+//	"github.com/harvester/harvester/tests/framework/dsl"
+//	"github.com/harvester/harvester/tests/integration/constant"
+//)
+//
+//var _ = ginkgo.Describe("verify node drain apis", func() {
+//	var node corev1.Node
+//	var nodeController ctlcorev1.NodeController
+//
+//	ginkgo.BeforeEach(func() {
+//		coreFactory, err := rancherCorev1.NewFactoryFromConfig(constant.KubeConfig)
+//		dsl.MustNotError(err)
+//		nodeController = coreFactory.Core().V1().Node()
+//
+//		gomega.Eventually(func() error {
+//			nodeList, err := nodeController.List(metav1.ListOptions{})
+//			if err != nil {
+//				return err
+//			}
+//			for _, v := range nodeList.Items {
+//				_, ok := v.Labels["node-role.kubernetes.io/control-plane"]
+//				if !ok {
+//					node = v
+//					return nil
+//				}
+//			}
+//			return fmt.Errorf("no worker node found in cluster")
+//		}).ShouldNot(gomega.HaveOccurred())
+//	})
+//
+//	ginkgo.It("drain node using drainhelper", func() {
+//		ginkgo.By("draining nodes", func() {
+//			err := drainhelper.DrainNode(constant.TestCtx, constant.KubeConfig, &node)
+//			dsl.MustNotError(err)
+//		})
+//
+//		gomega.Eventually(func() error {
+//			nodeObj, err := nodeController.Get(node.Name, metav1.GetOptions{})
+//			if err != nil {
+//				return err
+//			}
+//			if !nodeObj.Spec.Unschedulable {
+//				return fmt.Errorf("expected the node to be unschedulable")
+//			}
+//
+//			if len(nodeObj.Spec.Taints) == 0 {
+//				return fmt.Errorf("expected to find taints on the node")
+//			}
+//
+//			for _, v := range nodeObj.Spec.Taints {
+//				if v.Key == "node.kubernetes.io/unschedulable" && v.Effect == corev1.TaintEffectNoSchedule {
+//					return nil
+//				}
+//			}
+//
+//			return fmt.Errorf("expected to find taint for no schedule")
+//		}, "30s", "5s").ShouldNot(gomega.HaveOccurred())
+//
+//	})
+//})
